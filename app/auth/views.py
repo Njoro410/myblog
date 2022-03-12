52da10a76e884a2a563444dfeb5d074be29ce13e
njoro410 <brian.njoroge@student.moringaschool.com>
2022-03-12 23:06:09 +0300

    added materialize

--- app/auth/views.py
+++ app/auth/views.py
@@ -29,7 +29,6 @@ def login():
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user, login_form.remember.data)
             return redirect(request.args.get('next') or url_for('views.index'))
-        
         flash('uh oh, seems like your details were wrong')
         
     return render_template('auth/login.html', login_form=login_form)
--- app/views.py
+++ app/views.py
@@ -1,10 +1,16 @@
 from flask import Blueprint, render_template
 
+from app.request import get_quotes
+
 
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def index():
+    quote = get_quotes()
     
-    return render_template('index.html')
+    
+    
+    return render_template('index.html',quote=quote)
+
