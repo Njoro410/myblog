db28349805e24b429ac7e1aa7490aa1076643294
njoro410 <brian.njoroge@student.moringaschool.com>
2022-03-12 10:50:29 +0300

    created flask app, app configurations, registered blueprints

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from config import config_options
+
+def create_app(config_name):
+    
+    app = Flask(__name__)
+    
+    #creating app configurations
+    app.config.from_object(config_options[config_name])
+    
+    #register blueprint
+    from .views import views as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
+    
+    #initialize flask extensions
+    
+    return app
