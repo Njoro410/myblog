ad4b6ff04d1f2eead68d010fa43686683a86958f
njoro410 <brian.njoroge@student.moringaschool.com>
2022-03-12 23:01:41 +0300

    added class quote to store random quote instances

--- app/models.py
+++ app/models.py
@@ -9,7 +9,11 @@ import datetime
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
+class Quote():
+    def __init__(self,author,quote):
+        self.author = author
+        self.quote = quote
+    
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
