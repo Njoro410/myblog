5fa5e65a4f5efa14867d636ab073932eb201c351
njoro410 <brian.njoroge@student.moringaschool.com>
2022-03-14 01:54:49 +0300

    added a forms file for comments

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField
+from wtforms.validators import Required
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class CommentForm(FlaskForm):
+    content = TextAreaField('Comment',validators=[Required()])
+    submit = SubmitField('Submit')
