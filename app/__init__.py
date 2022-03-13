b146729064d9e9859384dc46ee7994cc925685a8
njoro410 <brian.njoroge@student.moringaschool.com>
2022-03-14 01:54:17 +0300

    imported and initialized flask uploads

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_materialize import Material
-
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
@@ -13,25 +13,30 @@ material = Material()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+photos = UploadSet('photos', IMAGES)
+
 
 def create_app(config_name):
-    
+
     app = Flask(__name__)
-    
-    #creating app configurations
+
+    # creating app configurations
     app.config.from_object(config_options[config_name])
-    
-    #register blueprint
+
+    # configure UploadSet
+    configure_uploads(app, photos)
+
+    # register blueprint
     from .views import views as main_blueprint
     app.register_blueprint(main_blueprint)
-    
+
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
-    
-    #initialize flask extensions
+
+    # initialize flask extensions
     db.init_app(app)
     login_manager.init_app(app)
     bootstrap.init_app(app)
     material.init_app(app)
-    
+
     return app
